65258f96a3c606dbb1459675b7f9e7834662f791
DeepakWorking <deepakpandey7789@gmail.com>
2022-12-29 12:35:14 +0530

    redirecting codebase click to provate git repo

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Button from '../../shared/Button'
 
 const Banner = ()=>{
+    const handleCodebase = ()=>{
+        window.open('https://github.com/DeepakWorking/spacex-capsules')
+    }
     return (
         <section className="banner">
             <h1>Assignment Project</h1>
             <p>Aim at showing qualification for the role</p>
-            <Button> Explore codebase</Button>
+            <Button onClick={()=>handleCodebase()}> Explore codebase</Button>
         </section>
     )
 }
